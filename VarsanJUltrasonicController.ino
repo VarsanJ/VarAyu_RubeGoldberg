93acf8bc8a804f83fa11b962bb116d0ee2fe14c4
Varsan <137824833+VarsanJ@users.noreply.github.com>
2025-06-21 21:53:26 -0400

    Update VarsanJUltrasonicController.ino

--- VarsanJUltrasonicController.ino
+++ VarsanJUltrasonicController.ino
@@ -21,23 +21,28 @@ int Position = 180;
 
 
 void setup() {
+  // Establishes input/output mode for each connected pin
   pinMode(ultrasonicTriggerpin, OUTPUT);
   pinMode(ultrasonicEchopin, INPUT);
   pinMode(servoMpin, OUTPUT);
+
+  // Connects servo motor and establishes serial for display
   servoM.attach(servoMpin);
-  Serial.begin(9600); //allows output
+  Serial.begin(9600); 
+
+  // Sets initial angle position for the servo
   servoM.write(180);
 }
 
 void loop() {
-  //Startup
+  //Startup, including establishing each pins state and delays help ensure code runs smoothly
   digitalWrite(ultrasonicTriggerpin, LOW);
   delayMicroseconds(2);
   digitalWrite(ultrasonicTriggerpin, HIGH);
   delayMicroseconds(5);
   digitalWrite(ultrasonicTriggerpin, LOW);
   
-  //Calculations
+  // Obtains the time, and uses the time and physics formula to determine distance from the sensor. Displays this information to serial for testing purposes
   Xtime = pulseIn(ultrasonicEchopin, HIGH); 
   Serial.print("Time is ");
   Serial.println(Xtime);
@@ -45,16 +50,16 @@ void loop() {
   Serial.print("Distance is ");
   Serial.println(distance);
 
-  //Conditionals
-  if (distance < 10)
+  // Primary functionality of the code, to run once object (the car) falls in front of the sensor
+  if (distance < 10) // Physically designed on the build itself
   {
     for (int i = 0; i<= 15; i++)
     {
       servoM.write(Position);
-      Position -= 6;
+      Position -= 6; // Modifies the positon in a for loop for a nice delayed rotation until the servo motor arm hits another ball
       Serial.println("Detected!}");
       delay(4000);
     }
-  servoM.write(180);  
+  servoM.write(180); // At end restores position to initial location 
   }
 }
